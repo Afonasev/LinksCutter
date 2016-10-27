6220c2c54e0756e84a3b100f9564b9d77105a6c7
Afonasev Evgeniy <afonasev@yandex-team.ru>
2016-10-27 12:24:12 +0500

    bugfix/api/get links route/page size params

--- backend/linkscutter/controllers/schemas.py
+++ backend/linkscutter/controllers/schemas.py
@@ -1,50 +1,56 @@
 
+get_links_route = {
+    'method': 'GET',
+    'route': '/api/v1/links?{page=%i}&{size=%i}',
+    'description': 'get list of links',
+    'responce_example': {
+        'page': 1,
+        'size': 20,
+        'count': 1,
+        'objects': [
+            {
+                'id': 1,
+                'url': 'www.site.com',
+                'key': 'aD2fvT',
+                'created_at': '2016-10-08T18:00:22',
+            },
+        ],
+    },
+}
+
+get_link_route = {
+    'method': 'GET',
+    'route': '/api/v1/links/{key}',
+    'description': 'get link by key',
+    'responce_example': {
+        'id': 1,
+        'url': 'www.site.com',
+        'key': 'aD2fvT',
+        'created_at': '2016-10-08T18:00:22',
+    },
+}
+
+create_link_route = {
+    'method': 'POST',
+    'route': '/api/v1/links',
+    'description': 'create new link',
+    'request_example': {
+        'url': 'www.site.com',
+    },
+    'responce_example': {
+        'id': 1,
+        'url': 'www.site.com',
+        'key': 'aD2fvT',
+        'created_at': '2016-10-08T18:00:22',
+    },
+}
+
 api_v1 = {
     'LinksCutter API v1': {
         'links': [
-            {
-                'method': 'GET',
-                'route': '/api/v1/links?{page=%i},{size=%i}',
-                'description': 'get list of links',
-                'responce_example': {
-                    'page': 1,
-                    'size': 20,
-                    'count': 1000,
-                    'objects': [
-                        {
-                            'id': 1,
-                            'url': 'www.site.com',
-                            'key': 'aD2fvT',
-                            'created_at': '2016-10-08T18:00:22',
-                        },
-                    ],
-                },
-            },
-            {
-                'method': 'GET',
-                'route': '/api/v1/links/{key}',
-                'description': 'get link by key',
-                'responce_example': {
-                    'id': 1,
-                    'url': 'www.site.com',
-                    'key': 'aD2fvT',
-                    'created_at': '2016-10-08T18:00:22',
-                },
-            },
-            {
-                'method': 'POST',
-                'route': '/api/v1/links',
-                'description': 'create new link',
-                'request_example': {
-                    'url': 'www.site.com',
-                },
-                'responce_example': {
-                    'id': 1,
-                    'url': 'www.site.com',
-                    'key': 'aD2fvT',
-                    'created_at': '2016-10-08T18:00:22',
-                },
-            },
+            get_links_route,
+            get_link_route,
+            create_link_route,
         ],
     },
 }
