e3c849a8018b41c8b872f493e279078b8d5cb28e
Afonasev Evgeniy <ea.afonasev@gmail.com>
2016-11-18 19:31:10 +0500

    conf/travis/skip_push_into_not_master_branch (#17)

--- backend/travis.sh
+++ backend/travis.sh
@@ -6,6 +6,6 @@ py.test --cov=./linkscutter
 flake8
 pylint linkscutter
 
-if ["$TRAVIS_EVENT_TYPE" = "push"] && ["$TRAVIS_BRANCH" = "master"]; then
-    coveralls;
-fi;
+if ["$TRAVIS_EVENT_TYPE" = "push"]; then
+    coveralls
+fi
