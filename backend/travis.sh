90a4d9d4c17fcbc97107ea77904e191310f07067
Afonasev Evgeniy <afonasev@yandex-team.ru>
2016-11-18 19:04:13 +0500

    conf/travis/check_pull_request_and_master_branch

--- backend/travis.sh
+++ backend/travis.sh
@@ -5,4 +5,7 @@ pip install -r requirements.txt
 py.test --cov=./linkscutter
 flake8
 pylint linkscutter
-coveralls
+
+if ["$TRAVIS_PULL_REQUEST" = "false"] && ["$TRAVIS_BRANCH" = "master"]; then
+    coveralls;
+fi;
