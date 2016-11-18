3112016e17010d1e33811d79873c60258e5d2cc7
Afonasev Evgeniy <ea.afonasev@gmail.com>
2016-11-18 19:11:33 +0500

    Conf/travis/check pull request and master branch (#16)

--- backend/travis.sh
+++ backend/travis.sh
@@ -5,4 +5,7 @@ pip install -r requirements.txt
 py.test --cov=./linkscutter
 flake8
 pylint linkscutter
-coveralls
+
+if ["$TRAVIS_EVENT_TYPE" = "push"] && ["$TRAVIS_BRANCH" = "master"]; then
+    coveralls;
+fi;
