e87f678fa7ccd16be07de2b98240580ea755f2df
Afonasev Evgeniy <afonasev@yandex-team.ru>
2016-11-18 19:05:45 +0500

    next

--- backend/travis.sh
+++ backend/travis.sh
@@ -6,6 +6,6 @@ py.test --cov=./linkscutter
 flake8
 pylint linkscutter
 
-if ["$TRAVIS_PULL_REQUEST" = "false"] && ["$TRAVIS_BRANCH" = "master"]; then
+if ["$TRAVIS_EVENT_TYPE" = "push"] && ["$TRAVIS_BRANCH" = "master"]; then
     coveralls;
 fi;
