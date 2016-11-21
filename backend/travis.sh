aaecb58b5ae9ea4be00058c98cb262efc0d0315c
Afonasev Evgeniy <afonasev@yandex-team.ru>
2016-11-21 20:01:13 +0500

    conf/travis/autodeploy

--- backend/travis.sh
+++ backend/travis.sh
@@ -8,7 +8,7 @@ pylint linkscutter
 
 push="push"
 
-if [ "$TRAVIS_EVENT_TYPE" == "$push" ]; then
+if [ "$TRAVIS_EVENT_TYPE" = "$push" ]; then
     coveralls
     apt-get install sshpass
     sshpass -e ssh root@138.68.65.124 -t supervisorctl restart linkscutter
